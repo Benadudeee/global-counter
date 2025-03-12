f99974ace9813f41349a9346de125432a3fe5320
BeneDUDE <applethug3244@gmail.com>
2025-03-12 18:01:27 -0400

    Initialized Flask-SQLAlchemy

--- src/main.py
+++ src/main.py
@@ -1,8 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Base(DeclarativeBase):
+    pass
 
 # To run flask --app main run
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('LOCAL_DB_URI')
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
 
 @app.route("/")
 def hello_world():
     return "<p> Hello Maven </p>"
+
+# For debug
+if __name__ == "__main__":
+    app.run(debug=True)
