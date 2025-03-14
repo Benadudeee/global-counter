c61cb3b4360fceb9745cc6a3be6c8f06ef4ec976
BeneDUDE <applethug3244@gmail.com>
2025-03-14 16:43:37 -0400

    Changed stuff

--- src/main.py
+++ src/main.py
@@ -1,54 +1,52 @@
-from flask import Flask 
-# render_template
-# from flask_socketio import SocketIO, emit
-# from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy.orm import DeclarativeBase
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
 
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# import json
+import json
 
 
-# load_dotenv()
-# class Base(DeclarativeBase):
-#     pass
+load_dotenv()
+class Base(DeclarativeBase):
+    pass
 
 # To run flask --app main run
 app = Flask(__name__)
 
 # ---- Config ----
-# app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('LOCAL_DB_URI')
-# app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('LOCAL_DB_URI')
+app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
 
-# socketio = SocketIO(app)
-# db = SQLAlchemy(model_class=Base)
-# db.init_app(app)
+socketio = SocketIO(app)
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
 
 
 
-# class Counter(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     amount = db.Column(db.Integer)
+class Counter(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Integer)
 
-# Creates a new counter
 
 @app.route("/")
 def hello_world():
-    # counter = Counter.query.filter_by(id=0).first()
+    counter = Counter.query.filter_by(id=0).first()
 
-    return "foo"
+    return render_template("home.html", counter=counter)
 
 
-# @socketio.on("count")
-# def increment_counter(count):
-#     counter = Counter.query.filter_by(id=0).first()
-#     counter.amount += 1
+@socketio.on("count")
+def increment_counter(count):
+    counter = Counter.query.filter_by(id=0).first()
+    counter.amount += 1
 
-#     db.session.commit()
+    db.session.commit()
 
-#     emit('count update', json.dumps({"count" : counter.amount}), broadcast=True)
+    emit('count update', json.dumps({"count" : counter.amount}), broadcast=True)
 
 # For debug
-# if __name__ == "__main__":
-#     app.run(app, debug=False)
+if __name__ == "__main__":
+    socketio.run(app, debug=False)
